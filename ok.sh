6685b397f1ae9c4cf9689925d88c782715418589
Abdul Mumit <mamumit07@gmail.com>
2016-02-14 01:48:50 +0600

    Remove echo

--- ok.sh
+++ ok.sh
@@ -10,8 +10,6 @@ filename="${1##*/}"
 extension="${filename##*.}"
 filebasename="${filename%.*}"
 
-echo $dir $filename $extension $filebasename
-
 if [ $extension == "cpp" ]; then
     g++ "$dir/$filename" -o "$dir/$filebasename.o" -std=c++11 -Wall && "$dir/$filebasename.o"
     rm -f "$dir/$filebasename.o"
