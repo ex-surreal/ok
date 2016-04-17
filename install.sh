fb94ea7a32c12347224d723f092b34310ef37980
Abdul Mumit <mamumit07@gmail.com>
2016-04-17 23:41:02 +0600

    Change directory implementation for setup

--- install.sh
+++ install.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
-mkdir -p ~/bin
-cat ok.sh > ~/bin/ok
-chmod +x ~/bin/ok
+mkdir -p $HOME/bin
+cat ok.sh > $HOME/bin/ok
+chmod +x $HOME/bin/ok
+if [ -f $HOME/.bash_profile ]; then
+    source $HOME/.bash_profile
+fi
 if ! [[ ":$PATH:" == *":$HOME/bin:"* ]]; then
-    echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bash_profile"
+    echo 'export PATH="$HOME/bin:$PATH"' >> "$HOME/.bash_profile" && echo 'PATH updated'
 fi
