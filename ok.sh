2edce14c1dee0424683fdcc4e5f83ed935133168
Abdul Mumit <mamumit07@gmail.com>
2016-04-17 23:41:17 +0600

    Add template freature for cpp

--- ok.sh
+++ ok.sh
@@ -1,5 +1,28 @@
 #!/bin/bash
 
+if [ "$1" == "-o" ]; then
+    echo -n '#include <bits/stdc++.h>
+
+using namespace std;
+
+#define mem(a, v) memset(a, v, sizeof (a))
+#define x first
+#define y second
+#define all(a) (a).begin(), (a).end()
+#define mp make_pair
+#define pb push_back
+#define sz(x) int((x).size())
+#define rep(i, n) for (int i = 0; i < int(n); i ++)
+#define repi(i, a, n) for (int i = (a); i < int(n); i ++)
+#define repe(x, v) for (auto x: (v))
+
+int main () {
+    std::ios_base::sync_with_stdio(false);
+    return 0;
+}' > "$2" && mvim "$2"
+exit 0
+fi
+
 if ! [ -f $1 ]; then
     echo "$1 file not found"
     exit 0
